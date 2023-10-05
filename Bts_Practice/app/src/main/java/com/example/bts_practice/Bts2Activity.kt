c4c15a99962c4af55866b1a1b73a43597a8edd55
JeongHyeon2 <81457699+JeongHyeon2@users.noreply.github.com>
2023-10-05 14:21:46 +0900

    Bts First App 여러 개의 사진중 하나를 누르면 사진이 확대되는 앱

---
+++ Bts_Practice/app/src/main/java/com/example/bts_practice/Bts2Activity.kt
@@ -0,0 +1,11 @@
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bts_practice.R
+
+class Bts2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bts2)
+    }
+}
