5480e8f0be8f35223cf032531982a19008a6f70b
JeongHyeon2 <81457699+JeongHyeon2@users.noreply.github.com>
2023-10-13 16:20:42 +0900

    [FEAT] RecyclerView 및 Glide

--- mango_contents/app/src/main/java/com/example/mango_contents/MainActivity.kt
+++ mango_contents/app/src/main/java/com/example/mango_contents/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.mango_contents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val items = mutableListOf<ContentsModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        for(i in 1..10) {
+            items.add(
+                ContentsModel(
+                    "https://www.mangoplate.com/restaurants/L-rthOXENI",
+                    "https://mp-seoul-image-production-s3.mangoplate.com/567429_1613885396044603.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                    "라룬비올렛"
+                )
+            )
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv)
+        recyclerView.adapter = RVAdapter(baseContext,items)
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
     }
 }
