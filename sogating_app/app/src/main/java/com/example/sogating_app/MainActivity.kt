2e8a43c3604f78c5d4a75fdc1b2ee0e2f4a7e005
jeonghyeon <3575253@naver.com>
2023-10-30 15:33:04 +0900

    [FEAT] 메인 슬라이딩 화면구현

--- sogating_app/app/src/main/java/com/example/sogating_app/MainActivity.kt
+++ sogating_app/app/src/main/java/com/example/sogating_app/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.sogating_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.sogating_app.slider.CardStackAdapter
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cardStackAdapter: CardStackAdapter
+    private lateinit var manager : CardStackLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardStackView = findViewById<CardStackView>(R.id.card_stack_view)
+        manager = CardStackLayoutManager(baseContext,object:CardStackListener{
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+
+            }
+
+            override fun onCardRewound() {
+
+            }
+
+            override fun onCardCanceled() {
+
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+
+            }
+
+        })
+        val testList = mutableListOf<String>("a","b","c")
+        cardStackAdapter = CardStackAdapter(baseContext,testList)
+
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdapter
+
+
     }
 }
