0833331e35fee66568e56016c76e1ff432440b02
jeonghyeon <3575253@naver.com>
2023-11-28 10:42:29 +0900

    [feat] bottom navi 구현

--- cooking_app/app/build.gradle.kts
+++ cooking_app/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.cooking_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cooking_app"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -41,7 +44,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Lottie
+    implementation ("com.airbnb.android:lottie:5.0.2")
 }
--- cooking_app/app/src/main/java/com/example/cooking_app/MainActivity.kt
+++ cooking_app/app/src/main/java/com/example/cooking_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.cooking_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cooking_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.fragmentContainer)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
